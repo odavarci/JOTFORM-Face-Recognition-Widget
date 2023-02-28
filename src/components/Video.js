0016e2e5476506fd64f7186dbe4ad5d894406176
odavarci <o.davarci@etu.edu.tr>
2023-02-28 08:56:28 +0300

    dummy message

--- src/components/Video.js
+++ src/components/Video.js
@@ -29,13 +29,14 @@ function Video(props) {
 
   jotform = window.JFCustomWidget;
   jotform.subscribe("ready", () => {
-    widgetFormID = jotform.formId();
-    console.log(widgetFormID);
+    //widgetFormID = jotform.formId();
+    //console.log(widgetFormID);
     let submissions = getResponses();
     submissions.then(function(response){
       faceArchiveSubmissions = response;
       setWidgetLoaded(true);  
     });
+    console.log(jotform);
   });
 
   useEffect(() => {
