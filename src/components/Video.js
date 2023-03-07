dd91a02150a74077046dd8c1df56549d64d40106
odavarci <o.davarci@etu.edu.tr>
2023-03-07 14:28:29 +0300

    dummy message

--- src/components/Video.js
+++ src/components/Video.js
@@ -259,7 +259,7 @@ function Video(props) {
     for(let i = 0; i < widgetQuestions.length; i++) {
       arr.push(widgetQuestions[i].qid);
     }
-    jotform.getFieldsValueById( widgetQuestions[0].qid, (response) => {
+    jotform.getFieldsValueById( arr, (response) => {
         // let input = response.data[0].value.split(" ");
         // submitFace(capturedFace, input[0], input[1]);
         // console.log("Submission sent");
