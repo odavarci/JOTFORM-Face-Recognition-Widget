e045169d28bf9bc9300121fc0c2217f4193dac91
odavarci <o.davarci@etu.edu.tr>
2023-03-13 19:20:18 +0300

    dummy message

--- src/components/Video.js
+++ src/components/Video.js
@@ -98,13 +98,13 @@ function Video(props) {
       formData.append('properties[title]', formID + "Database");
       formData.append('questions[0][type]', 'control_textbox');
       formData.append('questions[0][name]', faceFieldName);
-      //formData.append('questions[0][order]', '0');
+      formData.append('questions[0][order]', '0');
       for(let i = 0; i < widgetQuestions.length; i++) {
         console.log('questions[' + (i+1) + '][type]');
         console.log(widgetQuestions[i].qid.toString());
         formData.append('questions[' + (i+1) + '][type]', widgetQuestions[i].type.toString());
         formData.append('questions[' + (i+1) + '][name]', widgetQuestions[i].qid.toString());
-        //formData.append('questions[' + (i+1) + '][order]', (i+1).toString());
+        formData.append('questions[' + (i+1) + '][order]', '0');
       }
       axios.post('https://api.jotform.com/form?apiKey=' + apiKey, formData)
       .then(function(response){
