74df970778467e53bc8d28b1bf089795ffec6419
odavarci <o.davarci@etu.edu.tr>
2023-03-14 13:16:12 +0300

    dummy message

--- src/components/Video.js
+++ src/components/Video.js
@@ -307,32 +307,13 @@ function Video(props) {
   }
 
   function capture() {
-    //var canvas = document.getElementById("canvas");
-    //var video = document.querySelector("video");
-    var video = videoRef.current;
-    var canvas = canvasRef.current;
+    var canvas = document.getElementById("canvas");
+    var video = videoRef;
     canvas.width = videoWidth;
     canvas.height = videoHeight;
     canvas
       .getContext("2d")
       .drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  
-    /** Code to merge image **/
-    /** For instance, if I want to merge a play image on center of existing image **/
-    // const playImage = new Image();
-    // playImage.src = "path to image asset";
-    // playImage.onload = () => {
-    //   const startX = video.videoWidth / 2 - playImage.width / 2;
-    //   const startY = video.videoHeight / 2 - playImage.height / 2;
-    //   canvas
-    //     .getContext("2d")
-    //     .drawImage(playImage, startX, startY, playImage.width, playImage.height);
-    //   canvas.toBlob() = (blob) => {
-    //     const img = new Image();
-    //     img.src = window.URL.createObjectUrl(blob);
-    //   };
-    // };
-    /** End **/
   }
 
   const closeWebcam = () => {
@@ -416,6 +397,7 @@ function Video(props) {
 
   return (
     <Wrapper>
+        <canvas id="canvas"></canvas>
         {jotform.isWidgetOnBuilder() ?
           <h1>I am not working on builder :(</h1>
           :
