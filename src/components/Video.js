7badccf0a195e22e67d740838b0c2b6dd4c65221
odavarci <o.davarci@etu.edu.tr>
2023-03-20 14:38:40 +0300

    dummy message

--- src/components/Video.js
+++ src/components/Video.js
@@ -58,7 +58,7 @@ function Video(props) {
 
   const notRecognizedCallbackFunction = () => {
     createNewFaceSubmission(capturedFace);
-    //basicCallbackFunction();
+    basicCallbackFunction();
   }
   //----------------------------------------------------------------------------------------------------------
 
